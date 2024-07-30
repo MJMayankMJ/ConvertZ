fd3a0c8c981b5bf446b5c46dfb96b6f05b69a7d8
MJMayankMJ <226301131@gkv.ac.in>
2024-07-30 16:06:04 +0530

    Initial Commit

---
+++ Convertz/ConvertzApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConvertzApp.swift
+//  Convertz
+//
+//  Created by Mayank Jangid on 7/30/24.
+//
+
+import SwiftUI
+
+@main
+struct ConvertzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
