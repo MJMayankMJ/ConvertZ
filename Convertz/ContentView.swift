5ac072f614482900c73e764b6957e833f53f3f5f
MJMayankMJ <226301131@gkv.ac.in>
2024-07-30 17:16:08 +0530

    Initial Commit.

--- Convertz/ContentView.swift
+++ Convertz/ContentView.swift
@@ -8,14 +8,72 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    @State private var inputValue = 0.0
+    @State private var inputTemperatureType = "°C"
+    @State private var outputTemperatureType = "°F"
+    
+    let temperatureTypes = ["°C", "°F", "°K"]
+    var outputValue : Double {
+        
+        if inputTemperatureType == "°C" {
+            inputValue = inputValue
         }
-        .padding()
+        else if inputTemperatureType == "°F" {
+            inputValue = (inputValue - 32) * (5 / 9)
+        }
+        else if inputTemperatureType == "°K" {
+            inputValue = inputValue - 273.15
+        }
+        else {
+            print("error")
+        }
+        
+        if outputTemperatureType == "°C" {
+            return inputValue
+        }
+        else if outputTemperatureType == "°F" {
+            return ((inputValue * (9/5)) + 32)
+        }
+        else if outputTemperatureType == "°K" {
+            return (inputValue - 273.15)
+        }
+        else {
+            print("error")
+            return 0
+        }
+    }
+    
+    var body: some View {
+        NavigationStack {
+            Form{
+                Section("Input Temp."){
+                    HStack {
+                        TextField("Input Value", value: $inputValue, format: .number)
+                        Picker("Temp.", selection: $inputTemperatureType) {
+                            ForEach(temperatureTypes, id: \.self) {
+                                Text("\($0)")
+                            }
+                        }
+                        .pickerStyle(.segmented)
+                        
+                    }
+                }
+                Section("Output Temp."){
+                    HStack{
+                        Text(outputValue, format: .number)
+                        Spacer(minLength: 150)
+                        Picker("Temp.", selection: $outputTemperatureType) {
+                            ForEach(temperatureTypes, id: \.self) {
+                                Text("\($0)")
+                            }
+                        }
+                        .pickerStyle(.segmented)
+                        
+                    }
+                }
+            }
+            .navigationTitle("ConvertZ")
+        }
     }
 }
 
